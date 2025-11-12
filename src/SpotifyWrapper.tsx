705c0f20694e8ad5f3b3c2a3022ef3a546edc135
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-11-12 12:39:54 +0700

    useState

--- src/SpotifyWrapper.tsx
+++ src/SpotifyWrapper.tsx
@@ -1,10 +1,10 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
+import type { ITrack } from "./types/music.types";
 
 
-export function SpotifyWrapper({
-    children
-}: { children: ReactNode 
+export function SpotifyWrapper({children}: { children: ReactNode }) {
+    const [currentTrack, setCurrentTrack] = useState<ITrack | null>(null);
+    const [queue, setQueue] = useState<ITrack[]>([]);
 
-}) {
     return <div>{children}</div>
 }
